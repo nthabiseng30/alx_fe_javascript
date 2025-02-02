7de19038e9e407c46e3ca2809f4811d342706fb3
nthabiseng mahlangu <nthabisengmahlangu139@gmail.com>
2025-02-02 16:29:29 +0200

    Update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,15 +1,3 @@
-// Quote generator logic
-const quotes = [];
-const quoteDisplay = document.getElementById('quote-display');
-
-function addQuote() {
-const newQuoteText = document.getElementById('new-quote-text').value;
-const newQuoteCategory = document.getElementById('new-quote-category').value;
-quotes.push({ text: newQuoteText, category: newQuoteCategory });
-displayRandomQuote();
-saveQuotesToLocalStorage();
-}
-
 function displayRandomQuote() {
 const randomIndex = Math.floor(Math.random() * quotes.length);
 const randomQuote = quotes[randomIndex];
